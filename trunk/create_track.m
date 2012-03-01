708b5b4b5014c4672eeefb26787a2344504cba6e
ben <ben@c58d16e8-83a4-4b1d-9b52-8a7aa714ff45>
2012-03-01 22:31:02 +0000

    create track by looking at speed of track and angle of track

--- trunk/create_track.m
+++ trunk/create_track.m
@@ -17,11 +17,50 @@ function track = sub_create_track(point,frame,frames,d3_analysed)
 track(1).point=point;
 track(1).frame=frame;
 last_point = point;
-for f=1:length(frames)
+for f=2:length(frames)
   other_points = d3_analysed.unlabeled_bat{frames(f)};
   D = distance(last_point,other_points);
   [M p]=min(D);
-  track(end+1).point=other_points(p,:);
-  track(end).frame=frames(f);
-  last_point = track(end).point;
+  
+  [speed dir] = get_track_vel(track);
+  
+  if ~isempty(dir)
+    THETA = cart2pol(other_points(p,1),other_points(p,2));
+    
+    dir_diff = (THETA - dir(end));
+    if dir_diff > 2*pi
+      dir_diff = dir_diff - 2*pi;
+    elseif dir_diff < -2*pi
+      dir_diff = dir_diff + 2*pi;
+    end
+  end
+  
+  if isempty(speed) || (M < 1.8*speed(end) && dir_diff < 10*pi/180)
+    track(end+1).point=other_points(p,:);
+    track(end).frame=frames(f);
+    last_point = track(end).point;
+  else
+    return;
+  end
 end
+
+%in meters / frame
+function [sm_speed sm_dir] = get_track_vel(track)
+if length(track) >= 3
+  points = reshape([track(:).point],3,...
+    length([track(:).point])/3)';
+  frames = [track.frame]';
+  point_diff = diff(points);
+  speed = distance([0 0 0],point_diff) ./ abs(diff(frames));
+  sm_speed = smooth(speed);
+  
+  THETA = cart2pol(points(:,1),points(:,2));
+  sm_dir = smooth(unwrap(THETA));
+    
+  figure(10); 
+  subplot(2,1,1); plot(speed);
+  subplot(2,1,2); plot(unwrap(THETA));
+else
+  sm_speed=[];sm_dir=[];
+end
+
