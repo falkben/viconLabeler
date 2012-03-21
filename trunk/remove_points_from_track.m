c2b314e0e338ea8954cd123dafcb6a93c470eba7
ben <ben@c58d16e8-83a4-4b1d-9b52-8a7aa714ff45>
2012-03-21 16:54:10 +0000

    Older versions of matlab don't support the setOrder option in setdiff

--- trunk/remove_points_from_track.m
+++ trunk/remove_points_from_track.m
@@ -1,7 +1,8 @@
 function track_cell = remove_points_from_track(track_cell,points)
 track = track_cell.points;
 track_points=reshape([track(:).point],3,length([track(:).point])/3)';
-[b, m] = setdiff(track_points,points,'rows','stable');
+[b, m] = setdiff(track_points,points,'rows');
+m=sort(m);
 if ~isequal(m',1:length(track_points))
   track_cell.points = track(m);
 end
