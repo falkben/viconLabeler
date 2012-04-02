d9952011582f5008ec48396590f8facb97be222b
Ben Falk <falkben@users.noreply.github.com>
2012-04-02 21:41:44 +0000

    Now creates & saves d3analysed file from a c3d file

--- c3d2batmat.m
+++ c3d2batmat.m
@@ -1,6 +1,10 @@
-function c3d2batmat(pn,fn)
+function c3d2batmat(autosave,pn,fn)
 
-if nargin<2
+if nargin<1
+  autosave=1;
+end
+
+if nargin<3
   if ispref('vicon_labeler','c3d_files') && ...
       exist(getpref('vicon_labeler','c3d_files'),'dir')
     pn=getpref('vicon_labeler','c3d_files');
@@ -21,8 +25,66 @@ end
 
 c3d_trial=open_c3d(pn,fn);
 new_UB=crop_trial(c3d_trial.unlabeled_bat);
-[bat bat_avg plot_frames ignore_frames]=extract_bat_postion(new_UB);
+[bat_sm bat_avg plot_frames ignore_frames]=extract_bat_postion(new_UB);
 
+d3_analysed = create_d3_analysed(c3d_trial, bat_avg, new_UB, plot_frames, ignore_frames);
+
+save_d3_analysed(d3_analysed,autosave);
+
+
+
+function save_d3_analysed(d3_analysed,autosave)
+if ispref('vicon_labeler','processed_vicon') && ...
+    exist(getpref('vicon_labeler','processed_vicon'),'dir')
+  pn=getpref('vicon_labeler','processed_vicon');
+else
+  pn=uigetdir([],'Set the directory for your ratings file');
+  if pn~=0
+    setpref('vicon_labeler','processed_vicon',pn);
+  end
+end
+if ~isequal(pn,0)
+  setpref('vicon_labeler','processed_vicon',pn);
+  if autosave==0
+    [FileName,PathName] = uiputfile('.mat','Save ratings file',[pn '\' d3_analysed.trialcode '.mat']);
+  else
+    PathName=[pn '\'];
+    FileName=[d3_analysed.trialcode '.mat'];
+  end
+
+  if ~isequal(FileName,0) && ~isequal(PathName,0)
+    save([PathName FileName],'d3_analysed');
+    disp(['Saved File: ' [PathName FileName]]);
+  end
+
+end
+
+function d3_analysed=create_d3_analysed(c3d_trial, bat, UB, plot_frames, ignore_frames)
+
+d3_analysed = struct;
+
+d3_analysed.object(1).video = bat;
+d3_analysed.object(1).name = 'bat';
+d3_analysed.unlabeled_bat = UB;
+d3_analysed.fvideo = c3d_trial.frame_rate;
+
+fname=[c3d_trial.pn c3d_trial.fn];
+if strfind(fname,'/')
+  slashes = strfind(fname,'/');
+else
+  slashes = strfind(fname,'\');
+end
+datecode=fname(slashes(end-3)+1:slashes(end-2)-1);
+bat_name = fname(slashes(end-2)+1:slashes(end-1)-1);
+trial_num = fname(slashes(end)+6:slashes(end)+7);
+
+d3_analysed.trialcode = [bat_name '.' datecode '.' ...
+  num2str(trial_num,'%1.2d')];
+
+d3_analysed.startframe = plot_frames(1);
+d3_analysed.endframe = plot_frames(end);
+
+d3_analysed.ignore_segs = ignore_frames;
 
 
 
@@ -46,6 +108,7 @@ c3d_trial.unlabeled_bat = unlabeled_bat;
 c3d_trial.frame_rate = frame_rate;
 c3d_trial.start_f = start_f;
 c3d_trial.end_f = end_f;
+% c3d_trial.trig_frame = floor(trig_rt*frame_rate);
 c3d_trial.fn = fn;
 c3d_trial.pn = pn;
 
@@ -66,56 +129,57 @@ new_UB=cell(length(UB),1);
 
 for p = 1:size(UB{frame},1)
   last_point = UB{frame}(p,:);
-
+  
   other_bat_points=cell(length(UB),1);
-
+  
   for f=frame:length(frames)
     other_points=UB{f};
-
+    
     D = distance(other_points,last_point);
-
+    
     other_bat_points{f} = other_points(D<=thresh_distance,:);
-
+    
     curr_centroid = mean(other_bat_points{f},1);
     if ~isnan(curr_centroid(1))
       last_point = curr_centroid;
     end
-
+    
   end
-
+  
   last_point=UB{frame}(p,:);
   for f=frame-1:-1:1
     other_points=UB{f};
-
+    
     D = distance(other_points,last_point);
-
+    
     other_bat_points{f} = other_points(D<=thresh_distance,:);
-
+    
     curr_centroid = mean(other_bat_points{f},1);
     if ~isnan(curr_centroid(1))
       last_point = curr_centroid;
     end
   end
   
-%   if length(find(cellfun(@isempty,other_bat_points))) < length(find(cellfun(@isempty,new_UB)))
-%     new_UB=other_bat_points;
-%   end
-%   if sum(cellfun(@length,other_bat_points)) > sum(cellfun(@length,new_UB))
-%     new_UB=other_bat_points;
-%   end
-  if length(unique(cell2mat(other_bat_points),'rows')) > length(unique(cell2mat(new_UB),'rows'))
+  %   if length(find(cellfun(@isempty,other_bat_points))) < length(find(cellfun(@isempty,new_UB)))
+  %     new_UB=other_bat_points;
+  %   end
+  %   if sum(cellfun(@length,other_bat_points)) > sum(cellfun(@length,new_UB))
+  %     new_UB=other_bat_points;
+  %   end
+  if length(unique(cell2mat(other_bat_points),'rows')) > ...
+      length(unique(cell2mat(new_UB),'rows'))
     new_UB=other_bat_points;
   end
-
+  
 end
 
-all_bat=cell2mat(new_UB); 
+all_bat=cell2mat(new_UB);
 if isempty(all_bat)
   frame=randi(length(frames),1);
   new_UB=crop_trial(unlabeled_bat,frame);
   all_bat=cell2mat(new_UB);
 end
-figure(5); 
+figure(5);
 plot3(all_bat(:,1),all_bat(:,2),all_bat(:,3),'.k');
 axis equal;
 grid on;
@@ -147,11 +211,11 @@ for k=1:length(ignore_frames)
     eframe=plot_frames(end);
   else
     sframe=plot_frames(...
-    find(plot_frames<ignore_frames(k) & plot_frames>ignore_frames(k-1),1) ...
-    );
-  eframe=plot_frames(...
-    find(plot_frames<ignore_frames(k) & plot_frames>ignore_frames(k-1),1,'last') ...
-    );
+      find(plot_frames<ignore_frames(k) & plot_frames>ignore_frames(k-1),1) ...
+      );
+    eframe=plot_frames(...
+      find(plot_frames<ignore_frames(k) & plot_frames>ignore_frames(k-1),1,'last') ...
+      );
   end
   
   bat_sm(sframe:eframe,:)=[smooth(bat_avg(sframe:eframe,1)) ...
@@ -159,7 +223,7 @@ for k=1:length(ignore_frames)
     smooth(bat_avg(sframe:eframe,3))];
 end
 
-figure; plot3(bat_sm(:,1),bat_sm(:,2),bat_sm(:,3),'linewidth',3)
+figure(7); plot3(bat_sm(:,1),bat_sm(:,2),bat_sm(:,3),'linewidth',3)
 hold on; plot3(bat_avg(:,1),bat_avg(:,2),bat_avg(:,3),'r','linewidth',3)
 all_points=cell2mat(UB);
 plot3(all_points(:,1),all_points(:,2),all_points(:,3),'.k');
