8888f73deaaf16d6886adb66b5277f1b3d170cdf
Ben Falk <falkben@users.noreply.github.com>
2012-05-22 21:36:57 +0000

    keeps the before and after points plotting from previous trials axis equal now has a real effect

--- assign_labels.m
+++ assign_labels.m
@@ -222,9 +222,8 @@ set(handles.del_button,'enable','on');
 set(handles.labels_listbox,'enable','on');
 set(handles.prev_unlabeled,'enable','on');
 set(handles.all_labels_as_options,'enable','on');
+set(handles.labels_listbox,'Value',1);
 
-set(handles.pts_before,'string','0');
-set(handles.pts_after,'string','0');
 
 set(handles.figure1,'name',['Assign Labels: ' assign_labels.ratings_filename]);
 
@@ -306,8 +305,8 @@ text(trial_start_loc(1),trial_start_loc(2)-.2,trial_start_loc(3)+.2,...
 text(trial_end_loc(1)+.2,trial_end_loc(2),trial_end_loc(3)+.2,...
   'END');
 
-axis vis3d; axis equal;
-view([az,el]);
+% axis vis3d;
+view([az,el]); axis equal;
 grid on;
 
 figure(2);
@@ -337,8 +336,8 @@ else
     'START');
   text(track_points(end,1),track_points(end,2),track_points(end,3)+.15,...
     'END');
-  axis vis3d; axis equal;
-  view([az,el]);
+%   axis vis3d;
+  view([az,el]); axis equal;
   grid on;
 end
 
@@ -697,8 +696,8 @@ end
 h3=figure(3);clf;
 all_points=cell2mat(unlab_near_track);
 plot3(all_points(:,1),all_points(:,2),all_points(:,3),'.k');
-axis vis3d; axis equal;
-view([az,el]);
+% axis vis3d;
+view([az,el]); axis equal;
 a=axis;
 for k=1:length(plotting_frames)
   clf(h3); hold on;
