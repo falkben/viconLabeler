0b7ca9ec45a5dbf09ed82fdd4a8e18e06e1f497e
Ben Falk <falkben@users.noreply.github.com>
2015-03-04 19:13:43 +0000

    using weighted smoothed centroid instead of just a full smoothed centroid smoothing turn angle adjusted view during animation set axis before plot to lessen axis flashing during animation showing current track location on whole trial plot

--- calc_turn_angle.m
+++ calc_turn_angle.m
@@ -1,9 +1,10 @@
 %2d turn angle on smoothed centroid
-function turn_angle=calc_turn_angle(sm_C,DIAG)
+function turn_angle=calc_turn_angle(sm_C,frm_len,DIAG)
 
 dir_vec=diff(sm_C(:,1:2));
 turn_angle=unwrap(cart2pol(dir_vec(:,1),dir_vec(:,2))) .*180/pi;
 turn_angle(end+1)=turn_angle(end);
+turn_angle=smooth(turn_angle,frm_len);
 
 if nargin > 1 && DIAG
   
